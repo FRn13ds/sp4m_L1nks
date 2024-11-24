981898e6fed4be49e39c8ef0b2a936d4b7bed138
iamnot frn13ds <159894578+FRn13ds@users.noreply.github.com>
2024-11-24 13:48:05 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import time
 import requests
 from colorama import *
+import os
+
+os.system('cls')
 def spam_telegram_link(link, count):
     for i in range(count):
         response = requests.get(link)
@@ -11,5 +14,5 @@ if __name__ == "__main__":
     link_to_spam = input(Fore.RED+"Link To Spam :")
     number_of_visits = int(input(Fore.GREEN+"Number Of Visits :")) 
     spam_telegram_link(link_to_spam, number_of_visits)
-print(Fore.BLUE+"Your Order Placed Succsesfuly !")
+print(Fore.BLUE+"Finished Succsesfuly !")
 print(Fore.CYAN+Back.RED+"The Admin Says ",Fore.GREEN+"Thanks"," For Using His TOOL !",Style.RESET_ALL)
